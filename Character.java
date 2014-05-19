65472d55e0ae25a7d00792ce05413854248b1cb1
saiarvindg <sai.arvindg@gmail.com>
2014-05-19 11:01:39 -0400

    corrected methods

--- Character.java
+++ Character.java
@@ -1,15 +1,15 @@
 public abstract class Character{
   protected int dir = 0;
   protected int x,y;
-  protected boolean type = false; // true if player, false if guard
   
-  public void setDir(int dir_x, int dir_y){
-    x = dir_x;
-    y = dir_y;
+  
+  public void setDir(int d){
+    dir = d;
   }
   
-  public void setLoc(Location l){
-   loc = l; 
+  public void setLoc(int dir_x, int dir_y){
+   x = dir_x;
+   y = dir_y;
   }
   
   public void setType(boolean b){
