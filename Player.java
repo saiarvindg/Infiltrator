778b057558b696541f9b6d55f22846bca5e25804
saiarvindg <sai.arvindg@gmail.com>
2014-05-19 11:12:19 -0400

    created player class

---
+++ Player.java
@@ -0,0 +1,15 @@
+public class Player extends Character{
+  int x,y;
+  
+  public Player{
+    super.setLoc(0,0);
+    super.setDir(0);
+  }
+  
+  public void move(int nx, int ny){
+   
+    
+  }
+  
+  
+}
