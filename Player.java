ed78324f805a7f1ea62782469c2798f676614d6a
saiarvindg <sai.arvindg@gmail.com>
2014-05-20 11:12:56 -0400

    added more methods

--- Player.java
+++ Player.java
@@ -1,9 +1,15 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 public class Player extends Character{
   int x,y;
+  
   
   public Player{
     super.setLoc(0,0);
     super.setDir(0);
+    KeyListener kl = new KeyStuff();
+    addKeyListener(kl);
   }
   
   public void move(KeyEvent e){
@@ -11,16 +17,32 @@ public class Player extends Character{
     
     if(key == KeyEvent.VK_W){  //W key
       y -= 4;
+      super.setLoc(x,y);
     }
     if(key == KeyEvent.VK_S){   //S key
       y += 4;
+      super.setLoc(x,y);
     }
     if(key == KeyEvent.VK_A){   // A key
       x -= 4;
+      super.setLoc(x,y);
     }
     if(key == KeyEvent.VK_D){  //D key
       x += 4;
+      super.setLoc(x,y);
     }
   }
+  
+  public void paintComponent(Graphics g){
+    super.paintComponent(g);
+    
+  }
+  
+  private class KeyStuff implements KeyListener{
+    public void keyPressed(KeyEvent e){
+      int btn = 
+    }
+  }
+  
   
 }
