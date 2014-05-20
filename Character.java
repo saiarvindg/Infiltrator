be21ba666f4350cf246995ba2b1caf05135902bc
mpm165 <mikemcgth@gmail.com>
2014-05-20 10:39:41 -0400

    Character.java

--- Character.java
+++ Character.java
@@ -4,14 +4,27 @@ public abstract class Character{
   protected int x,y;
   
   public void setDir(int d){
-    dir = d;
+    dir = d/90;
   }
   
   public void setLoc(int new_x, int new_y){
-   x = new_x;
-   y = new_y;
-  }   
+    x = new_x;
+    y = new_y;
+  }
   
-  public void move();
-  //boxbug for guards
+  public void move(){
+    if(dir == 0)
+      y -= 2;
+    else if(dir == 1)
+      x += 2;
+    else if(dir == 2)
+      y += 2;
+    else if(dir == 3)
+      x -= 2;
+  }
+  public void turn(){
+    if(dir > 3)
+      dir = 0;
+    dir++;
+  }
 }
