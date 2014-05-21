9258940b03e3b8973a2b37f56a7369fb3e71ec26
saiarvindg <sai.arvindg@gmail.com>
2014-05-21 10:55:08 -0400

    changed move method

--- Player.java
+++ Player.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 
 public class Player extends Character implements KeyListener{
   
+    
   public Player{
     super.setLoc(0,0);
     KeyListener kl = new KeyListener();
@@ -18,24 +19,25 @@ public class Player extends Character implements KeyListener{
     
     if(key == KeyEvent.VK_W){  //W key
       super.y -= 4;
-      super.setLoc(x,y);
+      super.setLoc(super.x,super.y);
     }
     if(key == KeyEvent.VK_S){   //S key
       super.y += 4;
-      super.setLoc(x,y);
+      super.setLoc(super.x,super.y);
     }
     if(key == KeyEvent.VK_A){   // A key
       super.x -= 4;
-      super.setLoc(x,y);
+      super.setLoc(super.x,super.y);
     }
     if(key == KeyEvent.VK_D){  //D key
       super.x += 4;
-      super.setLoc(x,y);
+      super.setLoc(super.x,super.y);
     }
   }
   
   public void paintComponent(Graphics g){
     super.paintComponent(g);
+    g.fillOval
     
   }
   
