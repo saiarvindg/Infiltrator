e1746b0d24c8f646cb0ec10f63df47b2d2981260
saiarvindg <sai.arvindg@gmail.com>
2014-05-21 11:10:08 -0400

    created controlpanel

---
+++ CtrlPanel.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class CtrlPanel extends JPanel{
+  private JLabel timerDisp = new JLabel("Placeholder for Timer");
+  
+  public CtrlPanel(){
+   setBackground(Color.ORANGE); 
+  }
+}
