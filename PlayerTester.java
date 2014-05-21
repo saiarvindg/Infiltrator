0340d8d2b1db5418f4353a1ffde4044e74943d6e
saiarvindg <sai.arvindg@gmail.com>
2014-05-21 11:11:35 -0400

    created runner class to test player

---
+++ PlayerTester.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class PlayerTester {
+
+  public static void main(String[] args) { 
+    JFrame f = new JFrame();
+    f.setTitle("Player Tester");
+    f.setSize(500,500);
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    Container pane = f.getContentPane();
+    pane.setLayout( new BorderLayout() );
+    
+    
+  }
+   
+}
