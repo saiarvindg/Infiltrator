d4748fe521656fad8fb4f5cc851909194ce6501f
saiarvindg <sai.arvindg@gmail.com>
2014-05-21 10:58:08 -0400

    drew player in paintComponent

--- Player.java
+++ Player.java
@@ -20,25 +20,29 @@ public class Player extends Character implements KeyListener{
     if(key == KeyEvent.VK_W){  //W key
       super.y -= 4;
       super.setLoc(super.x,super.y);
+      repaint();
     }
     if(key == KeyEvent.VK_S){   //S key
       super.y += 4;
       super.setLoc(super.x,super.y);
+      repaint();
     }
     if(key == KeyEvent.VK_A){   // A key
       super.x -= 4;
       super.setLoc(super.x,super.y);
+      repaint();
     }
     if(key == KeyEvent.VK_D){  //D key
       super.x += 4;
       super.setLoc(super.x,super.y);
+      repaint();
     }
   }
   
   public void paintComponent(Graphics g){
     super.paintComponent(g);
-    g.fillOval
-    
+    g.fillOval(super.x, super.y, 8, 8);
+    g.setColor(Color.BLUE);
   }
   
   public void keyPressed(KeyEvent e){
