439a129aec1a15b8d46965986e87cf7f911a5382
mpm165 <mikemcgth@gmail.com>
2014-05-21 10:49:11 -0400

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,22 +1,10 @@
 public abstract class Character{
   
-  protected int dir = 0;
   protected int x,y;
-  
-  public void setDir(int d){
-    dir = d/90;
-  }
   
   public void setLoc(int new_x, int new_y){
     x = new_x;
     y = new_y;
   }
   
-  public abstract void move(){  } //Remove move method so sub-class can redfine it
-  
-  public void turn(){
-    if(dir > 3)
-      dir = 0;
-    dir++;
-  }
 }
