a1bd8dfce08db746ee08a2694f0918764ad23a6c
saiarvindg <sai.arvindg@gmail.com>
2014-05-22 10:40:55 -0400

    added characters

--- CtrlPanel.java
+++ CtrlPanel.java
@@ -6,6 +6,18 @@ public class CtrlPanel extends JPanel{
   private JLabel timerDisp = new JLabel("Placeholder for Timer");
   
   public CtrlPanel(){
+   setLayout(new BorderLayout());
    setBackground(Color.ORANGE); 
+   
+   add(timerDisp, BorderLayout.NORTH);
+   
+   Character p1 = new Player();
+   Guard g1 = new Guard();
+   g1.path(10,10,5,0);
+   
+   add(p1. BorderLayout.WEST);
+   add(g1. BorderLayout.EAST);
   }
+  
+  
 }
