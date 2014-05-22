18509aa0edd5d4129d0698036a8ccca1e2c07408
mpm165 <mikemcgth@gmail.com>
2014-05-22 10:40:47 -0400

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,10 +1,12 @@
-public abstract class Character{
+import javax.swing.*;
+
+public abstract class Character extends JPanel{
   
   protected int x,y;
   
   public void setLoc(int new_x, int new_y){
     x = new_x;
     y = new_y;
-  }  
+  }
   
 }
