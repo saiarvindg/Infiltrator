ee332b938448eb9eba921e9fbb641e61a3c51a91
saiarvindg <sai.arvindg@gmail.com>
2014-05-27 11:01:36 -0400

    made player move
    thank alex bock

--- InfiltratorControlPanel.java
+++ InfiltratorControlPanel.java
@@ -1,12 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.Timer;
 
 public class InfiltratorControlPanel extends JPanel{
   private JLabel timerDisp = new JLabel("Placeholder for Timer");
   private Player player;
   private Guard guard;
-  private javax.swing.Timer timer;
+  private Timer timer;
   
   public InfiltratorControlPanel(){
    setLayout(new BorderLayout());
@@ -15,11 +16,15 @@ public class InfiltratorControlPanel extends JPanel{
    add(timerDisp, BorderLayout.NORTH);
    
    player = new Player();
-   guard = new Guard();
-   guard.path(100,100,50,5);
+   guard = new Guard(100,100,2,0);
+   
+   addKeyListener(player);
    
    timer = new javax.swing.Timer(1000, new TimerListener());
    timer.start();
+   
+   setFocusable(true);
+   //setFocus(true);
   }
   
   public void paintComponent(Graphics g){
