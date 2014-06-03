8f4fc49e0b8daa78135dbd283bc908e80c567fdd
mpm165 <mikemcgth@gmail.com>
2014-06-03 11:03:19 -0400

    Update Map.java

--- Map.java
+++ Map.java
@@ -3,23 +3,25 @@ import java.util.ArrayList;
 
 public class Map extends JPanel{
   
-  public ArrayList<walls> m;
+  public ArrayList<Walls> m;
   
   public Map(){
-    m = new ArrayList<walls>();
+    m = new ArrayList<Walls>();
 
     //Border Walls
-    m.add(new walls(0,0,50,5));
-    m.add(new walls(0,0,5,50));
-    m.add(new walls(50,0,550,25));
-    m.add(new walls(0,50,25,550));
-    m.add(new walls(25,575,575,25));
-    m.add(new walls(575,0,25,600));
+    m.add(new Walls(0,0,50,5));
+    m.add(new Walls(0,0,5,50));
+    m.add(new Walls(50,0,550,25));
+    m.add(new Walls(0,50,25,550));
+    m.add(new Walls(25,575,525,25));
+    m.add(new Walls(575,0,25,600));
        
-    m.add(new walls(200,200,200,25));
-    m.add(new walls(200,200,25,175));
-    m.add(new walls(400,200,25,200));
-    m.add(new walls(225,400,200,25));
-    m.add(new walls(25,50,25,25));
+    m.add(new Walls(200,200,200,25));
+    m.add(new Walls(200,200,25,175));
+    m.add(new Walls(400,200,25,200));
+    m.add(new Walls(225,400,200,25));
+    m.add(new Walls(25,50,25,25));
+    m.add(new Walls(50,100,25,400));
+    m.add(new Walls(50,500,75,25));
   }
 }
