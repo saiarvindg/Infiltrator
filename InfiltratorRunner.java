424b6424a900f438d7b4c4f3cef254d6d1ae3225
saiarvindg <sai.arvindg@gmail.com>
2014-06-05 11:10:11 -0400

    changed dimsensions and setResizable to false

--- InfiltratorRunner.java
+++ InfiltratorRunner.java
@@ -2,16 +2,19 @@ import javax.swing.*;
 import java.awt.*;
 
 public class InfiltratorRunner {
+    
   public static void main(String[] args) {
     JFrame f = new JFrame("InfiltratorRunner");
     f.setSize(616,638);
     f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     Container pane = f.getContentPane();
     f.setLayout(new BorderLayout());
+    f.setResizable(false);
     
     InfiltratorControlPanel ICP = new InfiltratorControlPanel();
     pane.add(ICP);
 
     f.setVisible(true);
   }
+ 
 }
