0660dbb762cd89817062cbc71815dc3d41e070c4
saiarvindg <sai.arvindg@gmail.com>
2014-06-05 11:09:09 -0400

    fixed detection system

--- InfiltratorControlPanel.java
+++ InfiltratorControlPanel.java
@@ -5,13 +5,13 @@ import javax.swing.Timer;
 import java.util.ArrayList;
 
 public class InfiltratorControlPanel extends JPanel{
-  private JLabel timerDisp = new JLabel("Time: 20");
+  private JLabel timerDisp = new JLabel("Time: 30");
   private JButton gameOver = new JButton();
   private Player player;
   private ArrayList<Guard> guards;
   private Timer timer;
   private Timer detectTimer;
-  private int countdown = 20;
+  private int countdown = 200;
   private Map map;
   
   public InfiltratorControlPanel(){
@@ -23,22 +23,23 @@ public class InfiltratorControlPanel extends JPanel{
     gameOver.setVisible(false);
     gameOver.addActionListener( new playAgain() );
     
-    player = new Player(this);
+    map = new Map();
+    player = new Player(this, map);
     guards = new ArrayList<Guard>();
-    for(int c =1; c < 11; c++){
-      int rndx = (int)(500*Math.random());
+    for(int c =1; c < 12; c++){
+      int rndx = (int)(475*Math.random()) + 75;
       int rndy = (int)(400*Math.random()) + 100;
-      guards.add(new Guard(rndx,rndy,5,0));
+      guards.add(new Guard(rndx+c,rndy+c,5,0));
     }
     
     addKeyListener(player);
     
     timer = new Timer(1000, new TimerListener());
-    detectTimer = new Timer(1000, new GuardDetect());
+    detectTimer = new Timer(100, new GuardDetect());
     timer.start();
     detectTimer.start();
     
-    map = new Map();
+    
  
     setFocusable(true);
   }
@@ -74,7 +75,7 @@ public class InfiltratorControlPanel extends JPanel{
   public boolean detect(Player p, Guard grd){
     for(int i = 0; i<16; i++){
       for(int s = 0 ; s<16; s++){
-        if((grd.p).contains(p.x+i, p.y+s))
+        if((grd.p).intersects(p.x+i, p.y+s, 20, 20))
           return true; 
       }
     }
@@ -90,14 +91,16 @@ public class InfiltratorControlPanel extends JPanel{
         gd.move();
       }
       
-      /*if(gameOver(guards, countdown)){
+      /*
+      if(gameOver(guards, countdown)){
        timer.stop();
        detectTimer.stop();
        removeKeyListener(player);
        gameOver.setVisible(true);
        gameOver.setEnabled(true);
        gameOver.setText("GAME OVER. CLICK TO PLAY AGAIN");      
-       }*/
+       }
+      */
       
       repaint();
     }
@@ -119,7 +122,23 @@ public class InfiltratorControlPanel extends JPanel{
   private class playAgain implements ActionListener{
     public void actionPerformed(ActionEvent e){
       gameOver.setText("UNDER PROGRESS. IN THE MEANWHILE, PONDER THIS QUOTE: \n SPEAK SOFTLY & CARRY A BIG STICK");
-      //replay();
+      replay();
     }
   }
+  
+  public void replay(){
+    countdown = 200;
+    gameOver.setVisible(false);
+    gameOver.setEnabled(false);
+    //gameOver.addActionListener( new playAgain() );
+    player.x = 15;
+    player.y = 15;
+    player.pdx = 3;
+    player.pdy = 3;
+    addKeyListener(player);
+    
+    timer.start();
+    detectTimer.start();
+  }
+  
 }
