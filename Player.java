3e2bc8f5133a65ca44be02d3810cd83c062ee9ed
saiarvindg <sai.arvindg@gmail.com>
2014-06-05 10:34:10 -0400

    udpated

--- Player.java
+++ Player.java
@@ -6,35 +6,52 @@ import java.awt.event.KeyListener;
 
 public class Player extends Character implements KeyListener{
   private JPanel display;
+  public int pdx,pdy;
+  private Map mp;
   
-  public Player( JPanel jp ){
+  public Player( JPanel jp, Map pm ){
     display = jp;
+    pdx = 3;
+    pdy = 3;
     super.setLoc(15,15);
+    mp = pm;
   }
   
   public void move(KeyEvent e){
     int key = e.getKeyCode();
     System.out.println(super.x + ", " + super.y);
     if(key == KeyEvent.VK_W){
-      super.y -= 4;
+      super.y -= pdy;
       super.setLoc(super.x,super.y);
       display.repaint();
     }
     if(key == KeyEvent.VK_S){
-      super.y += 4;
+      super.y += pdy;
       super.setLoc(super.x,super.y);
       display.repaint();
     }
     if(key == KeyEvent.VK_A){
-      super.x -= 4;
+      super.x -= pdx;
       super.setLoc(super.x,super.y);
       display.repaint();
     }
     if(key == KeyEvent.VK_D){
-      super.x += 4;
+      super.x += pdx;
       super.setLoc(super.x,super.y);
       display.repaint();
     }
+    
+    for(Walls w: mp.m){
+      if(display.contains(w.x, w.y, w.l, w.h)){
+        //gdx *= -1;
+       // gdy *= -1;
+        break;
+      }
+      else
+        walk();
+      break;
+    }
+    
   }
   
   public void draw(Graphics g){
